1ced02d46c3b32dc52b9dc72dd977e5aa48a9859
thirax <fluffy@PC-Gamer.localdomain>
2020-05-10 15:26:38 +0200

    Formulaire  fait finalment

--- Projet/app/controllers/HomeController.java
+++ Projet/app/controllers/HomeController.java
@@ -10,12 +10,13 @@ import play.mvc.*;
 
 
 
-public class HomeController extends Controller {
 
-    //nique  sa  mere a java
-    /*
+public class HomeController extends Controller {
 @Inject FormFactory formFactory;
 Form<User> userForm;
+    //nique  sa  mere a java
+    /*
+
 
     public Result sayhelloform(){
         userForm = formFactory.form(User.class);
@@ -34,11 +35,49 @@ Form<User> userForm;
         return ok(views.html.index.render());
     }
     
-    public Result test(String fname) {
-        return ok(views.html.test.render(fname));
+    public Result test(Http.Request request) {
+        DynamicForm requestData = formFactory.form().bindFromRequest(request);
+        String pseudo = requestData.get("pseudo");
+        String password = requestData.get("password");
+        return ok(views.html.test.render(pseudo));
     }
 
     public Result login() {
-        return ok(views.html.login.render());
+        return ok("azer");//views.html.login.render());
     }
+
+    public Result loginBIS() {
+
+        userForm = formFactory.form(User.class);
+            // case class UserData(pseudo: String, password: String)   
+        return ok(views.html.login.render(userForm));
+    }   
+    
+    
+
+    
+    
+    /*
+    public Result identification() {  // statique  normalement
+        Form<Login> loginForm  = form(login.class).bindFromRequest();
+
+        //a  voir
+            if (loginForm.hasErrors()) {
+        return badRequest(views.html.login.render(loginForm));
+    } else {
+        session().clear();
+        session("email", loginForm.get().email);
+        return redirect(
+            routes.Application.index()
+        );
+    }
+    }
+
+    public String validation() {
+        if(User.identification(pseudo)==null){
+            return "mdp/user invalide"
+        }
+        return null;
+    }
+    */
 }
--- Projet/app/models/User.java
+++ Projet/app/models/User.java
@@ -4,9 +4,27 @@ import javax.persistence.*;
 import com.avaje.ebean.Model;
 import play.data.validation.*;
 */
+
+import play.libs.Files.TemporaryFile;
+import play.mvc.Http.MultipartFormData.FilePart;
+
+
 public class User {
-	public String pseudo, mdpHash; 
-	boolean admin = false;
-	//?
-	// moult var  a ajouter
-}
+	public String pseudo, password; 
+    /*
+      public void setPassword(String password) {
+        this.password = password;
+      }
+
+      public String getPassword() {
+        return password;
+      }
+      public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+      }
+
+      public String getPseudo() {
+        return pseudo;
+    }
+    */
+  }
