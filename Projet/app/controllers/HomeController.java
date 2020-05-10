9484df774d31e95f2a71e3271b768dae7c23080a
thirax <fluffy@PC-Gamer.localdomain>
2020-05-10 23:51:39 +0200

    fix  du bug profils

--- Projet/app/controllers/HomeController.java
+++ Projet/app/controllers/HomeController.java
@@ -20,25 +20,23 @@ public class HomeController extends Controller {
     }
     
     public Result profile(Http.Request request) {
-        if(request != null){  //on verifie si on invoque la page avec des data ou pas  pour eviter qu'il parse  des data nul 
-            //si  il   n'y  a   pas  de  soucis on va  convertire  la requete en  formulaire
-            Form<User> formulaireRecus = userForm.bindFromRequest(request); 
-            //on  cherche si  le  formulaire a  des erreur  voir User.java
-            if (formulaireRecus.hasErrors()) {
-                //si  il y a  une erreur allor on renvoit le  precedant  formulaire
-                return badRequest(views.html.login.render(userForm)); //marche pas,  on a  un renvoit  de  page  maispas   de  formulaire 
-            }
-            else{
-                //si  tout ce passe  bien allor on  va crée un  object  user qui a  les data  du formulaire
-                User userProfils = formulaireRecus.get();
-                //on  balance  un  ok avec  un get
-                return ok(views.html.profile.render(userProfils));   
-            }    
+        //on déclare userForm avec une valleur  sinonça   fait crash 
+        userForm = formFactory.form(User.class);
+        //si  il   n'y  a   pas  de  soucis on va  convertire  la requete en  formulaire
+        Form<User> formulaireRecus = userForm.bindFromRequest(request); 
+        //on  cherche si  le  formulaire a  des erreur  voir User.java
+        if (formulaireRecus.hasErrors()) {
+            //si  il y a  une erreur allor on renvoit le  precedant  formulaire
+            return badRequest(views.html.login.render(userForm)); //marche pas,  on a  un renvoit  de  page  maispas   de  formulaire 
         }
         else{
-            //si  le formulaire est vide   allor on  fait le   rendu de la page de login
-            return badRequest(views.html.login.render(userForm));
+            //si  tout ce passe  bien allor on  va crée un  object  user qui a  les data  du formulaire
+            User userProfils = formulaireRecus.get();
+            //on  balance  un  ok avec  un get
+            return ok(views.html.profile.render(userProfils));   
+                
         }
+
     }
 
     public Result login() {
