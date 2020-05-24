9633d03f93b0905d130ca86fcd36b3889f0a39dc
thirax <fluffy@PC-Gamer.localdomain>
2020-05-24 03:06:49 +0200

    ajout suport base de donnée

--- Projet/app/models/User.java
+++ Projet/app/models/User.java
@@ -1,38 +1,59 @@
 package models;
 import play.api.data.*;
-import play.libs.Files.TemporaryFile;
-import play.mvc.Http.MultipartFormData.FilePart;
+import play.data.validation.*;
 import play.data.validation.Constraints.* ;
+//import play.libs.Files.TemporaryFile;
+//import play.mvc.Http.MultipartFormData.FilePart;
 
-public class User {
+import io.ebean.*;
+import java.util.*;
+import javax.persistence.*;
+
+@Entity
+public class User extends Model{
+    
+    private static final long serialVersionUID= 1L;
+    
+    @Id
+    public long id;
+    
+    public static Finder<Long, User> find = new Finder<Long,User>(User.class);
+    
+    
+    //definition des Prérequis  par champ
     
     @Required
     @Pattern(value = "^[A-Za-z0-9 ]{1,24}$",    message = "sale bz on  est plus en 2010")
+	private String pseudo;
     //verifier si pas  existant dans la  bd  ?
     
-	private String pseudo;
     @Required
-    @Pattern(value = "^(?=.{8,})(?=.*[A-Z]+)(?=.*[0-9]+).*",    message = "kikko detected")//MDP qui contient au moins un chiffre et une majuscule et qui va entre 8 et 32 caracteres
-    
+    @Pattern(value = "^(?=.{8,})(?=.*[A-Z]+)(?=.*[0-9]+).*",    message = "kikko detected")//au moins un chiffre et une majuscule et qui va entre 8 et 32 caracteres
     private String password; 
     
         public User(){
             
         }
     
-      public void setPassword(String password) {
+    
+    //Déclaration accesseur  est Seter
+    
+    //faire status connecter  deconecter
+    
+    
+    public void setPassword(String password) {
         this.password = password;
-      }
+    }
 
-      public String getPassword() {
+    public String getPassword() {
         return password;
-      }
-      public void setPseudo(String pseudo) {
-        this.pseudo = pseudo;
-      }
-
-      public String getPseudo() {
-        return pseudo;
     }
     
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
   }
