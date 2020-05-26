672a8f8a2d004770aa29a433f6ca141efa91441b
thirax <fluffy@PC-Gamer.localdomain>
2020-05-26 12:31:40 +0200

    e

---
+++ Projet/app/controllers/Jeuxapp.java
@@ -0,0 +1,83 @@
+package controllers;
+
+//CLEANER !!
+
+import models.*;
+import play.mvc.*;
+import play.data.*;
+import views.html.*;
+import java.util.List;
+import javax.inject.Inject;
+import play.i18n.MessagesApi;
+
+
+public class Jeuxapp extends Controller {
+    
+    @Inject
+    FormFactory formFactory;
+    MessagesApi messagesApi;
+    Form<Jeux> jeuxForm;
+
+    @Inject
+    public Jeuxapp(FormFactory formFactory, MessagesApi messagesApi){
+        this.jeuxForm = formFactory.form(Jeux.class);
+        this.messagesApi = messagesApi;
+    }
+
+
+
+    public Result newgame(Http.Request request) {
+//        User joueur1 = User.find.byId(idjoueur1);
+//        User joueur2 = User.find.byId(idjoueur2);
+        return ok();
+    }
+
+    //création d'une partie
+//    public Result newgame(Long idjoueur1,Long idjoueur2) {
+//        User joueur1 = User.find.byId(idjoueur1);
+//        User joueur2 = User.find.byId(idjoueur2);
+//        return ok();
+//    }
+
+    //Page après envoi formulaire
+    public Result resultatnewgame(Http.Request request) {
+        Form<Jeux> cForm = jeuxForm.bindFromRequest(request);
+        //Si erreur réafficher la page contact avec les messages d'erreur
+        if (cForm.hasErrors()) {
+            return badRequest("bad request");//views.html.Jeux.game.render(cForm, null, request, messagesApi.preferred(request)));
+        }
+        //Sinon afficher la page contact avec message stipulant que le message a bien été envoyé.
+        else{
+            Jeux a = cForm.get();
+            a.save();
+            return ok("resultatnewgamefait");//views.html.Jeux.game.render(cForm, "Votre requête nous a bien été transmise et sera traitée dès que possible. Merci !", request, messagesApi.preferred(request)));
+        }
+    }
+
+    //formulaire pour  une action
+//    public Result formulaireactionloby(Http.Request request) {
+//        jeuxForm = formFactory.form(Jeux.class);
+//        return ok(views.html.Jeux.game.render(jeuxForm, request,messagesApi.preferred(request)));
+//    }
+
+    //Valide  l'action du joueur dans le jeux
+//    public Result checkformulaireactionloby(Http.Request request) {
+//        Form<Jeux> formulaireRecus = jeuxForm.bindFromRequest(request);
+//            Jeux tour = formulaireRecus.get();
+//            tour.save();
+//            return ok(views.html.User.profile.render(tour));
+//        }
+
+
+
+
+//    public Result lobyliste() {
+//        List<Jeux> liste = Jeux.find.all();
+//        return ok(views.html.Jeux.liste.render(liste));
+//    }
+//
+//    public Result loby(Long idloby) {
+//        Jeux loby = Jeux.find.byId(idloby);
+//        return ok(Jeux.resumerJoueur());
+//    }
+}
