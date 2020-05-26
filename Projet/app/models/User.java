a8d7a1139aaa0167e3501a0feba5eba18c7da15c
thirax <fluffy@PC-Gamer.localdomain>
2020-05-26 17:59:57 +0200

    model user  mis  a jour

--- Projet/app/models/User.java
+++ Projet/app/models/User.java
@@ -13,37 +13,47 @@ import javax.persistence.*;
 public class User extends Model{
     
     private static final long serialVersionUID= 1L;
-    private ArrayList<int> points = new ArrayList<int>; // permetrait de stocker temporairement les points/score pendant une competition...
+    private ArrayList<Integer> points = new ArrayList<Integer>(); // permetrait de stocker temporairement les points/score pendant une competition...
 	
     @Id
     public long id;
     
     public static Finder<Long, User> find = new Finder<Long,User>(User.class);
-    
-    
-    
-    
+
+
+    private boolean status;//true = admin false = normal
+
+
+
     //definition des Prérequis  par champ
-    
     @Required
     @Pattern(value = "^[A-Za-z0-9 ]{1,24}$",    message = "sale bz on  est plus en 2010")
 	private String pseudo;
+
     //verifier si pas  existant dans la  bd  ?
-    
     @Required
     @Pattern(value = "^(?=.{8,})(?=.*[A-Z]+)(?=.*[0-9]+).*",    message = "kikko detected")//au moins un chiffre et une majuscule et qui va entre 8 et 32 caracteres
-    private String password; 
-    
+    private String password;
+
         public User(){
-            
+
         }
-    
-    
+
+
+
     //Déclaration accesseur  est Seter
-    
+
     //faire status connecter  deconecter
-    
-    
+
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
     public void setPassword(String password) {
         this.password = password;
     }
@@ -51,7 +61,7 @@ public class User extends Model{
     public String getPassword() {
         return password;
     }
-    
+
     public void setPseudo(String pseudo) {
         this.pseudo = pseudo;
     }
@@ -71,9 +81,9 @@ public class User extends Model{
 		points.add(n);
 	}
 	
-	public String SommePoints(){
+	public int sommePoints(){
 		int pts = 0;
-		for ( String p: points ) {
+		for ( int p: points ) {
 		   pts += p;
 		}
 		return pts;
