e4dbedc6dc372f578fdb71238a0bd2bc1e5ca089
Nicolas | SlaughtY <64400217+SlaughtyY@users.noreply.github.com>
2020-05-31 17:54:38 +0200

    Peaufinage

--- Projet/app/controllers/Userapp.java
+++ Projet/app/controllers/Userapp.java
@@ -29,43 +29,42 @@ public class Userapp extends Controller {
     }
 
 
-    //profile  est embigue il permet de loader la page profile est verifie les formuler de register
+    //Profil  est ambigu, il permet de charger la page profil et vérifie les formulaires d'enregistrement
     public Result profile(Http.Request request) {
-        //on déclare userForm avec une valleur  sinon ça   fait crash
-        //si  il   n'y  a   pas  de  soucis on va  convertire  la requete en  formulaire
+        //On déclare userForm avec une valeur  sinon ça fait crash
+        //Si  il   n'y  a   pas  de  soucis on va  convertir  la requête en  formulaire
         Form<User> formulaireRecus = userForm.bindFromRequest(request);
-        //On  vérifie si  le  formulaire a  des erreur  voir User.java
+        //On  vérifie si  le  formulaire à  des erreur  voir User.java
         if (formulaireRecus.hasErrors()) {
             if(request.session().get("session").isPresent()){//verification si kookies
                 User userProfils = User.find.byId(Long.parseLong((request.session().get("session").get())));
                 return ok(views.html.User.profile.render(userProfils));
             }
-            //on verrrifie  si  le  formulaire est  vide
+            //On vérifie  si  le  formulaire est  vide
             if(formulaireRecus.toString().equals("Form(of=class models.User, data={}, value=Optional[pseudo: nullidentifiant: 0], errors=[ValidationError(password,[error.required],[]), ValidationError(pseudo,[error.required],[])])")||
-               formulaireRecus.toString().equals("Form(of=class models.User, data={}, value=Optional[pseudo: nullidentifiant: 0], errors=[ValidationError(pseudo,[error.required],[]), ValidationError(password,[error.required],[])])")){//si le formulaire est vide
+               formulaireRecus.toString().equals("Form(of=class models.User, data={}, value=Optional[pseudo: nullidentifiant: 0], errors=[ValidationError(pseudo,[error.required],[]), ValidationError(password,[error.required],[])])")){//Si le formulaire est vide
                 return redirect("/login");
             }
-            //S'il y a  une erreur alors on renvoit le formulaire ou pas de  formulaire
-            return badRequest(views.html.User.register.render(formulaireRecus, request,messagesApi.preferred(request))); //marche pas,  on a  un renvoit  de  page  maispas   de  formulaire
+            //S'il y a  une erreur alors on renvoit le formulaire
+            return badRequest(views.html.User.register.render(formulaireRecus, request,messagesApi.preferred(request))); //Marche pas,  on a  un renvoie  de  page  mais pas   de  formulaire
         }
         else{
-
-            //Si tout ce passe bien alors on va créer un object user qui a les data du formulaire
+            //Si tout ce passe bien alors on va créer un objet user qui a les data du formulaire
             User userProfils = formulaireRecus.get();
-            //on va  sauvgarder le les data dans  la  base de  donnée
+            //On va  sauvegarder les données dans  la  base de donnée
             userProfils.save();
-            //Oon  balance un ok avec un get
+            //On  balance un ok avec un get
             return ok(views.html.User.profile.render(userProfils));
-                                            //.addingToSession(request, "session", String.valueOf(truc.id));//on ajoute un kookie qui a pour id de session l'id de l'user en sachant que le mieux ces un truc  random
-
+            //.addingToSession(request, "session", String.valueOf(truc.id));//On ajoute un dookie qui a pour id de session l'id de l'user en sachant que le mieux c'est un truc aléatoire
+            
             //redirect("userliste");
         }
 
     }
 
-
+    //Page de connexion
     public Result login(Http.Request request) {
-        //On créé un formulaire a  partire de User
+        //On créé un formulaire à partir de User
         userForm = formFactory.form(User.class);
         //On envoit le formulaire dans checklogin
         return ok(views.html.User.login.render(userForm, request,messagesApi.preferred(request)));
@@ -74,15 +73,15 @@ public class Userapp extends Controller {
     public Result checklogin(Http.Request request) {
         Form<User> formulaireRecus = userForm.bindFromRequest(request);
         if (formulaireRecus.hasErrors()) {
-            return badRequest(views.html.User.login.render(formulaireRecus, request,messagesApi.preferred(request))); //marche pas,  on a  un renvoit  de  page  maispas   de  formulaire
+            return badRequest(views.html.User.login.render(formulaireRecus, request,messagesApi.preferred(request))); //Marche pas,  on a  un renvoie  de  page  mais pas   de  formulaire
         }
         else{
             User userProfils = formulaireRecus.get();
-            List<User> u = User.find.all(); // type inference works here!
+            List<User> u = User.find.all(); //Type inference works here!
             for(User truc : u) {
                 if (truc.getPseudo().equals(userProfils.getPseudo())){
                     if(truc.getPassword().equals(userProfils.getPassword())){
-                        return redirect("/profile").addingToSession(request, "session", String.valueOf(truc.id));//on ajoute un kookie qui a pour id de session l'id de l'user en sachant que le mieux ces un truc  random
+                        return redirect("/profile").addingToSession(request, "session", String.valueOf(truc.id));//On ajoute un cookie qui a pour id de session l'id de l'user en sachant que le mieux c'est un truc  aléatoire
                     }
                 }
             }
@@ -93,38 +92,42 @@ public class Userapp extends Controller {
 
     }
 
-    //Déconnexion
+    //Déconnexion en créant une nouvelle session vierge
     public Result unlog(Http.Request request) {
         return ok(views.html.User.unlog.render()).withNewSession();
 }
 
+    //Page d'enregistrement
     public Result register(Http.Request request) {
-        //On créé un formulaire a  partire de User
+        //On créé un formulaire a  partir de User
         userForm = formFactory.form(User.class);
-        //On envoit le formulaire dans login
+        //On envoie le formulaire dans login
         return ok(views.html.User.register.render(userForm, request,messagesApi.preferred(request)));
     }
 
-
+    //Rendu de la liste des joueurs
     public Result userlist() {
         List<User> liste = User.find.all();
         return ok(views.html.User.liste.render(liste));
     } 
     
+    //Suppression d'un utilisateur avec redirection sur la liste des joueurs
     public Result delete(Long id) {
         User u = User.find.byId(id);
         u.delete();
-        return ok("user  Suprimer");
+        return redirect(routes.Userapp.userlist());
     }
     
+    //Vidange de toute la BDD de joueurs avec redirection sur la liste des joueurs
     public Result flush() {
-        List<User> u = User.find.all(); // type inference works here!
+        List<User> u = User.find.all(); //Type inference works here!
         for(User truc : u) {
             truc.delete();
         }
-        return ok("tout le monde est mort / Suprimer");
+        return redirect(routes.Userapp.userlist());
     }
     
+    //Formulaire de modification de profil basique acessible depuis le profil
     public Result updateuser(Http.Request request) {
         User u = User.find.byId(Long.parseLong((request.session().get("session").get()))) ;
         Form <User> uForm = formFactory.form(User.class) ;
@@ -132,6 +135,7 @@ public class Userapp extends Controller {
         return ok(views.html.User.updateuser.render(u, uForm, request, messagesApi.preferred(request))) ;
     }
     
+    //Envoie du formulaire de modification de profil basique acessible depuis le profil qui mettra la BDD à jour
     public Result updateuserform(Http.Request request) {
         final Form<User> uForm = userForm.bindFromRequest(request);
         User u = uForm.get();
@@ -140,6 +144,7 @@ public class Userapp extends Controller {
         return redirect(routes.Userapp.profile());
     }
     
+    //???
     public Result nbjoueur() {
         List<User> liste = User.find.all();
         return ok(views.html.Var.nbjoueur.render(liste));
@@ -161,4 +166,10 @@ public class Userapp extends Controller {
         u.update();
         return redirect(routes.Userapp.userlist());
     }
+    
+    //Génération du panneau d'administration en fonction du grade de l'utilisateur
+    public Result panneladmin(Http.Request request) {
+        User u = User.find.byId(Long.parseLong((request.session().get("session").get())));
+        return ok(views.html.User.panneladmin.render(u));
+    }    
 }
