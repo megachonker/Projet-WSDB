b924ce197d8542daedee3610a2db06ac1b21ccd1
Nicolas | SlaughtY <64400217+SlaughtyY@users.noreply.github.com>
2020-05-31 17:44:15 +0200

    Peaufinage

--- Projet/app/models/User.java
+++ Projet/app/models/User.java
@@ -9,7 +9,7 @@ import play.data.validation.Constraints.* ;
 public class User extends Model{
     
     private static final long serialVersionUID= 1L;
-    private ArrayList<Integer> points = new ArrayList<Integer>(); // permetrait de stocker temporairement les points/score pendant une competition...
+    private ArrayList<Integer> points = new ArrayList<Integer>(); // Permetrait de stocker temporairement les points/score pendant un match
 	
     @Id
     public long id;
@@ -17,18 +17,18 @@ public class User extends Model{
     public static Finder<Long, User> find = new Finder<Long,User>(User.class);
 
 
-    private boolean status;//true = admin false = normal
+    private boolean status;//True = Admin False = normal
 
 
 
-    //definition des Prérequis  par champ
+    //Définition des pré-requis
     @Required
-    @Pattern(value = "^[A-Za-z0-9 ]{1,24}$",    message = "sale bz on  est plus en 2010")
+    @Pattern(value = "^[A-Za-z0-9 ]{1,24}$",    message = "Pseudo non valable")
 	private String pseudo;
 
-    //verifier si pas  existant dans la  bd  ?
+    //Vérification si pas existant dans la bd  ?
     @Required
-    @Pattern(value = "^(?=.{8,})(?=.*[A-Z]+)(?=.*[0-9]+).*",    message = "kikko detected")//au moins un chiffre et une majuscule et qui va entre 8 et 32 caracteres
+    @Pattern(value = "^(?=.{8,})(?=.*[A-Z]+)(?=.*[0-9]+).*",    message = "Mot de passe pas assez fort")//Au moins un chiffre, une majuscule et qui va entre 8 et 32 caractères
     private String password;
 
     //Grade de l'utilisateur
@@ -42,8 +42,9 @@ public class User extends Model{
 
 
 
-    //Déclaration accesseur  est Seter
+    //Liste des getters et setters
 
+    //Liste des getters et setters pour l'ID
     public long getId() {
         return id;
     }
@@ -52,6 +53,7 @@ public class User extends Model{
         this.id = iden;
     }
 	
+    //Liste des getters et setters pour le grade
     public String getGrade() {
         return grade;
     }
@@ -59,10 +61,8 @@ public class User extends Model{
     public void setGrade(String grade) {
         this.grade = grade;
     }
-	
-    //faire status connecter  deconecter
 
-
+    //Liste des getters et setters pour le statut    
     public boolean getStatus() {
         return status;
     }
@@ -71,6 +71,7 @@ public class User extends Model{
         this.status = status;
     }
 
+    //Liste des getters et setters pour le mot de passe    
     public void setPassword(String password) {
         this.password = password;
     }
@@ -79,6 +80,7 @@ public class User extends Model{
         return password;
     }
 
+    //Liste des getters et setters pour le pseudo    
     public void setPseudo(String pseudo) {
         this.pseudo = pseudo;
     }
@@ -87,17 +89,22 @@ public class User extends Model{
         return pseudo;
     }
     
+    //Méthode toString   
     public String toString() {
         return "pseudo: " + pseudo+ "identifiant: "+id;
     }
+    
+    //Réinitilise les points
 	public void setPointZero(){
 		points.clear();
 	}
 	
+    //Ajoute des points
 	public void setPoint(int n){
 		points.add(n);
 	}
-	
+    
+    //Méthode calcule de points
 	public int sommePoints(){
 		int pts = 0;
 		for ( int p: points ) {
