b6e4eb87d16641ceebdcc6cad007ea9e59bab8b6
Nicolas | SlaughtY <slaughtyy@users.noreply.github.com>
2020-06-17 09:36:02 +0200

    Petites modifications (Supression de ligne de codes commentés dans jeuxapp,  correction soucis redirection création de jeux)

--- Projet/app/controllers/Jeuxapp.java
+++ Projet/app/controllers/Jeuxapp.java
@@ -25,50 +25,40 @@ public class Jeuxapp extends Controller {
         this.messagesApi = messagesApi;
     }
 
-    //On affichage de la  page pour une nouvel  partie
+    //On affichage la page de création de partie
     public Result newgame(Http.Request request) {
-        //on test que  le mec  qui crée la  game est user  id  1
-        //ici  on part du  principe  qu'on est  admin  pour  debugage
-        if (true){ //((request.session().get("session").get()).equals("1")){//on verifi en fonction de l'id donc  faudra plus tard chercher dans la  bd
             List<User> liste = User.find.all();
             User u = User.find.byId(Long.parseLong((request.session().get("session").get()))) ;
             return ok(views.html.Jeux.makeloby.render(u, liste, jeuxForm,request, messagesApi.preferred(request)));
         }
-        else {
-            //message derreur qui  redirige ver page type
-            return ok(views.html.messagetempo.render("Vous n'ète pas admin ===> Dégage !"));
-        }
-    }
 
-	
+    //On affichage la page des salons de jeu
     public Result lobyliste() {
         List<Jeux> liste = Jeux.find.all();
         return ok(listloby.render(liste));
     }
 
-
-    //Page a la récéption du formulaire
+    //Page a la récéption du formulaire de création de partie
     public Result resultatnewgame(Http.Request request) {
-        //on  remplie le formulaire
+        //On remplit le formulaire
         Form<Jeux> cForm = jeuxForm.bindFromRequest(request);
-        //Si erreur réafficher la page contact avec les messages d'erreur
+        //Si erreur, réafficher la page de création de partie avec les messages d'erreur
         if (cForm.hasErrors()) {
              List<User> liste = User.find.all();
             User u = User.find.byId(Long.parseLong((request.session().get("session").get()))) ;
-            return badRequest(views.html.Jeux.makeloby.render(u, liste, cForm, request,messagesApi.preferred(request))); //Marche pas,  on a  un renvoie  de  page  mais pas   de  formulaire
-//            return badRequest("ERROR bad request ;:"+cForm.toString());//views.html.Jeux.game.render(cForm, null, request, messagesApi.preferred(request)));
+            return badRequest(views.html.Jeux.makeloby.render(u, liste, cForm, request,messagesApi.preferred(request))); //Marche pas, on a un renvoit de page mais pas de  formulaire
         }
-        //Sinon afficher la page contact avec message stipulant que le message a bien été envoyé.
+        //Si pas erreur, on traite le formulaire
         else{
-            //on transmet les variable du formulaire dans  jeux
+            //On transmet les variables du formulaire dans jeux
             Jeux magame = cForm.get();
 
-            //setUser1   peut  porter a  confusion  car  ici ces  l'id  d'user  qui est dans  les  loby
+            //setUser1 peut porter à confusion, car ici c'est l'id d'user qui est dans les salons
 
-            //On va  dumper tout la base de donnée dans une liste car les requette ne fonctionne pas
+            //On va dumper tout la base de donnée dans une liste car les requêtes ne fonctionnent pas
             List<User> machin = User.find.all();
 
-                //verifie si les champ sont vide
+            //Verifie si les champs sont vide
             if(magame.getPseudo1().equals("")){
                 magame.setUser1(n--);
             }
@@ -76,10 +66,10 @@ public class Jeuxapp extends Controller {
                 magame.setUser2(n--);
             }
 
-            for(User selecUser : machin) {  // on  va  lister chaque user
+            for(User selecUser : machin) {  // On va lister chaque user
 
-                if (selecUser.getPseudo().equals(magame.getPseudo1())) {//si l'utilisateur qui a le droit  de rejoindre la game existe dans la bd
-                    magame.setUser1((int) selecUser.id);//on va chercher a stoquer l'identifiant de l'user demander pour  la  wheitlsliete
+                if (selecUser.getPseudo().equals(magame.getPseudo1())) {//Si l'utilisateur qui a le droit  de rejoindre la game existe dans la BDD
+                    magame.setUser1((int) selecUser.id);//On va chercher à stocker l'identifiant de l'user demandé pour la  whiteliste
                 }
                 if (selecUser.getPseudo().equals(magame.getPseudo2())) {
                     magame.setUser2((int)selecUser.id);
@@ -92,52 +82,43 @@ public class Jeuxapp extends Controller {
             }
 
             magame.save();
-            return redirect("/");//views.html.Jeux.game.render(cForm, "Votre requête nous a bien été transmise et sera traitée dès que possible. Merci !", request, messagesApi.preferred(request)));
-//            return redirect("/"+magame.id);//views.html.Jeux.game.render(cForm, "Votre requête nous a bien été transmise et sera traitée dès que possible. Merci !", request, messagesApi.preferred(request)));
+            return redirect("/panneladmin");
         }
     }
-//    public Result validsearch(long id){
-//        return redirect("/lobyjoin/"+id);
-//    }
 
     public Result joinlobyAffichage(long id){
         return ok("views.html.Jeux.");
 
     }
 
-	public Result joinloby(Http.Request request,long id){ //join loby verrifi que l'utilisateur peut renter  mais est aussi la  main du game
+	public Result joinloby(Http.Request request,long id){ //Join lobby vérifie que l'utilisateur peut rentrer mais est aussi la main du game
 
         if (request.session().get("session").isEmpty()){
-//
-//            return ok(views.html.messagetempo.render("Session en cours de déconnexion... À très bientôt ! ♥")).withNewSession();
-//            return redirect("/anonymisation");
             User actualuser = new User("guest");
             actualuser.save();
-//            return ok("fait").addingToSession(request, "session", String.valueOf(actualuser.id));
             return redirect("/lobyjoin/"+id).addingToSession(request, "session", String.valueOf(actualuser.id));
         }
-        long idvisiteur  = Long.parseLong((request.session().get("session").get()));//avec le cookie on   cherche l'id de  l'user
+        long idvisiteur  = Long.parseLong((request.session().get("session").get()));//Avec le cookie on   cherche l'id de l'user
 
-        Jeux loby = Jeux.find.byId(id); //cherche le loby dans la bd en fonction de l'uri
-        User actualuser = User.find.byId(idvisiteur);// j'isole la  classe user
-//        return ok(""+loby.toString());
+        Jeux loby = Jeux.find.byId(id); //Cherche le lobby dans la BDD en fonction de l'id
+        User actualuser = User.find.byId(idvisiteur);// J'isole la  classe user
 
-        if(loby.getUser1()<0 && (int)idvisiteur!=loby.getUser2()){ // es  id de  place libre
+        if(loby.getUser1()<0 && (int)idvisiteur!=loby.getUser2()){ // Es id de place libre
 
             loby.id =  id;
-            loby.setUser1((int)idvisiteur);//peut  etre   erreur ?
+            loby.setUser1((int)idvisiteur);//Peut-être erreur ?
             loby.setPseudo1(actualuser.getPseudo());
-            loby.update();//2foix en  double peut opti
+            loby.update();//2fois en double peut opti
             return ok(views.html.Jeux.game.render("joueur 1",loby, actualuser, request, messagesApi.preferred(request)));
         }
         else if(loby.getUser2()<0 && loby.getUser1()!=(int)idvisiteur){
-            loby.setUser2((int)idvisiteur);//peut  etre   erreur ?
+            loby.setUser2((int)idvisiteur);//Peut-être erreur ?
             loby.setPseudo2(actualuser.getPseudo());
             loby.update();
             return ok(views.html.Jeux.game.render("joueur 2",loby, actualuser, request, messagesApi.preferred(request)));
         }
 
-        if(idvisiteur  == (loby.getUser1())){  //EQUALSE !! //que l'on va comparer avec les id autoriser du loby
+        if(idvisiteur  == (loby.getUser1())){  //EQUALSE !! //Que l'on va comparer avec les id autorisés du lobby
             return ok(views.html.Jeux.game.render("joueur 1",loby, actualuser, request, messagesApi.preferred(request)));
         }
         else if(idvisiteur  == (loby.getUser2())){
@@ -155,13 +136,8 @@ public class Jeuxapp extends Controller {
         return ok("action valide");
     }
 
-//    public Result checkactionloby(Http.Request request) {
-//        jeuxForm = formFactory.form(Jeux.class);
-//        return redirect(/);
-//    }
-
     public Result flush() {
-        List<Jeux> u = Jeux.find.all(); // type inference works here!
+        List<Jeux> u = Jeux.find.all(); //Type inference works here!
         for(Jeux truc : u) {
             truc.delete();
         }
